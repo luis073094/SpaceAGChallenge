ddd12db77cb039a9964a5b61cd2ce0eb6d97b5a4
Luis Mundaca <luis073094@gmail.com>
2021-08-29 00:58:11 -0500

    Final

--- SpaceAGChallenge/SpaceAGChallenge/FieldWorker/tests.py
+++ SpaceAGChallenge/SpaceAGChallenge/FieldWorker/tests.py
@@ -13,19 +13,28 @@ class FieldWorkerAPITestCase(APITestCase):
                 "last_name": "Mundaca", "function": "Harvest"}
 
         response = self.client.post("/v1/field_workers/", data)
-
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+
 
     def test_update(self):
-        id = "b64b8cdd-8075-493c-adb4-f80c95f8c1bf"
-        data = {"first_name": "LuisPrueba",
+        data = {"first_name": "Luis",
                 "last_name": "Mundaca", "function": "Harvest"}
+        
+        response = self.client.post("/v1/field_workers/", data)
+        jsonResponse = json.loads(response.content)
+        id = jsonResponse["id"]
 
-        response = self.client.patch("/v1/field_workers/" + id, data)
-
+        response = self.client.patch("/v1/field_workers/" + id + "/", data)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+
 
     def test_delete(self):
-        id = "b64b8cdd-8075-493c-adb4-f80c95f8c1bf"
-        response = self.client.delete("/v1/field_workers/" + id)
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        data = {"first_name": "Luis",
+                "last_name": "Mundaca", "function": "Harvest"}
+        
+        response = self.client.post("/v1/field_workers/", data)
+        jsonResponse = json.loads(response.content)
+        id = jsonResponse["id"]
+
+        response = self.client.delete("/v1/field_workers/" + id + "/")
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
