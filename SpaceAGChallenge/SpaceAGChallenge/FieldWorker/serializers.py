0277774ed8dc314471711b1cf7b7d84a8f060f6e
Luis Mundaca <luis073094@gmail.com>
2021-09-01 15:23:20 -0500

    observaciones en validaciones

--- SpaceAGChallenge/SpaceAGChallenge/FieldWorker/serializers.py
+++ SpaceAGChallenge/SpaceAGChallenge/FieldWorker/serializers.py
@@ -1,8 +1,33 @@
-from FieldWorker.models import FieldWorker
+import re
+
 from rest_framework import serializers
+
+from FieldWorker.models import FieldWorker
 
 
 class FieldWorkerSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = FieldWorker
         fields = ['id', 'first_name', 'last_name', 'function', 'created_at']
+        read_only_fields = ['id', 'created_at']
+
+    def validate_first_name(self, value):
+        value = re.sub('[^A-Za-z0-9 ]+', '', value.strip())
+        if value is None or value == "":
+            raise serializers.ValidationError("first_name cannot be null")
+
+        return value
+
+    def validate_last_name(self, value):
+        value = re.sub('[^A-Za-z0-9 ]+', '', value.strip())
+        if value is None or value == "":
+            raise serializers.ValidationError("last_name cannot be null")
+
+        return value
+
+    def validate_function(self, value):
+        value = value.strip()
+        if value.upper() != 'HARVEST' and value.upper() != 'PRUNNING' and value.upper() != 'SCOUTING' and value.upper() != 'OTHER':
+            value = 'Other'
+
+        return value
--- SpaceAGChallenge/SpaceAGChallenge/FieldWorker/views.py
+++ SpaceAGChallenge/SpaceAGChallenge/FieldWorker/views.py
@@ -1,61 +1,27 @@
-from django.shortcuts import render
-from FieldWorker.models import FieldWorker
 from rest_framework import viewsets
+
+from FieldWorker.models import FieldWorker
 from FieldWorker.serializers import FieldWorkerSerializer
-from rest_framework.decorators import action
-from rest_framework.response import Response
-import re
-from rest_framework.exceptions import APIException
+
 
 class WorkerViewSet(viewsets.ModelViewSet):
     queryset = FieldWorker.objects.all()
     serializer_class = FieldWorkerSerializer
 
+    """
     def create(self, request):
-        function = request.data['function']
-        function = function.strip()
+        serializer = FieldWorkerSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
 
-        if function.upper() != 'HARVEST' and function.upper() != 'PRUNNING' and function.upper() != 'SCOUTING' and function.upper() != 'OTHER':
-            function = 'Other'
-
-        vfirst_name = re.sub('[^A-Za-z0-9 ]+', '', request.data['first_name'].strip())
-        if vfirst_name is None or vfirst_name == "":
-            raise APIException("first_name cannot be null")
-
-        vlast_name = re.sub('[^A-Za-z0-9 ]+', '', request.data['last_name'].strip())
-        if vlast_name is None or vlast_name == "":
-            raise APIException("last_name cannot be null")
-        
-
-        worker = FieldWorker.objects.create(
-            first_name = vfirst_name,
-            last_name = vlast_name,
-            function = function
-        )
-        serializer = self.get_serializer(worker)
-        return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def partial_update(self, request, pk=None):
-        worker = self.get_object()
+        serializer = FieldWorkerSerializer(self.get_object(), data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
 
-        function = request.data.get('function', worker.function)
-        function = function.strip()
-
-        if function.upper() != 'HARVEST' and function.upper() != 'PRUNNING' and function.upper() != 'SCOUTING' and function.upper() != 'OTHER':
-            function = 'Other'
-
-        vfirst_name = re.sub('[^A-Za-z0-9 ]+', '', request.data.get('first_name', worker.first_name))
-        if vfirst_name is None or vfirst_name == "":
-            raise APIException("first_name cannot be null")
-
-        vlast_name = re.sub('[^A-Za-z0-9 ]+', '', request.data.get('last_name', worker.last_name))
-        if vlast_name is None or vlast_name == "":
-            raise APIException("last_name cannot be null")
-
-        worker.first_name = vfirst_name
-        worker.last_name = vlast_name
-        worker.function = function
-        worker.save()
-
-        serializer = self.get_serializer(worker)
-        return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    """
